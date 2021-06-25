93d9dbdad3da17e5b1046ecb51ec46f6fdb1d5bd
tim33-23 <tim33-23@mail.ru>
2021-06-25 03:56:35 +0300

    добавление профиллировщика

--- config/bundles.php
+++ config/bundles.php
@@ -7,5 +7,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
